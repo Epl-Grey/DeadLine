0a1d1093a7c9e1a2f9f0ba5b46b939cd768ea3ed
a4ifka <ilyha.borec@gmail.com>
2022-11-04 11:21:47 +0300

    add Intent

--- app/src/main/java/com/example/deadline/MainActivity.java
+++ app/src/main/java/com/example/deadline/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.deadline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button adding;
+    Intent addIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adding =(Button) findViewById(R.id.adding);
+        addIntent = new Intent(this, AddingActivity.class);
+        adding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(addIntent);
+            }
+        });
+
     }
 }
