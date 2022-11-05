a485f0d82daecbc4fad4587c260fd6caaf2f43d9
a4ifka <ilyha.borec@gmail.com>
2022-11-05 10:49:37 +0300

    +

---
+++ app/src/main/java/com/example/deadline/InfoActivity.java
@@ -0,0 +1,59 @@
+package com.example.deadline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class InfoActivity extends AppCompatActivity {
+
+    TextView name;
+    TextView data;
+    TextView time;
+    TextView info;
+
+    DatabaseHelper sqlHelper;
+    SQLiteDatabase db;
+    Cursor userCursor;
+    long userId=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        name = findViewById(R.id.name);
+        data = findViewById(R.id.data);
+        time = findViewById(R.id.time);
+        info = findViewById(R.id.info);
+
+        sqlHelper = new DatabaseHelper(this);
+        db = sqlHelper.getWritableDatabase();
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            userId = extras.getLong("id");
+        }
+
+        userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE + " where " +
+                DatabaseHelper.COLUMN_ID + "=?", new String[]{String.valueOf(userId)});
+        userCursor.moveToFirst();
+        name.setText(userCursor.getString(1));
+        data.setText(userCursor.getString(2));
+        time.setText(userCursor.getString(3));
+        info.setText(userCursor.getString(4));
+        userCursor.close();
+
+
+    }
+
+    public void backDoor(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
